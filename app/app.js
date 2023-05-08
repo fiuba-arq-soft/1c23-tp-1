e73f1f7fa299ae42117a55fc29d89409428db780
franco <franco@flydreamers.com>
2023-05-08 13:18:21 -0300

    Implementación de endpoints

--- app/app.js
+++ app/app.js
@@ -6,10 +6,13 @@ import axios from "axios";
 
 import { createClient } from "redis";
 
+import { XMLParser } from "fast-xml-parser";
+import { decode } from "metar-decoder";
+
 /* 
+
 const rateLimit = require('express-rate-limit')
 
-const { XMLParser } = require('fast-xml-parser');
 const { decode } = require('metar-decoder');
 
 const apiLimiter = rateLimit({
@@ -44,76 +47,98 @@ app.get("/", async (req, res) => {
   res.status(200).send("ping");
 });
 
-async function getFacts() {
-  const factString = await redisClient.get("fact");
-  if (factString !== NULL) {
-    return {res: JSON.parse(factString), status:200, errorMessage:""};
+/**
+ * Gets or set random fact information
+ * @returns
+ */
+async function getFact() {
+  let fact_string = await redisClient.get("fact");
+
+  if (fact_string !== null) {
+    return JSON.parse(fact_string);
   } else {
     try {
-      const factRes = await axios.get(
+      let facts_res = await axios.get(
         "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
       );
-      redisClient.set("fact", JSON.stringify(factRes.text), { EX: 15 });
-      return { res: fact, status: 200, errorMessage:"" };
-
-    } catch (error) {
-      return {
-        res: null,
-        status: error.status,
-        errorMessage: error.message,
-      };
+      const facts_info = facts_res.data;
+      let fact = facts_info.text;
+      redisClient.set("fact", JSON.stringify(fact), { EX: 60 }).then(() => {
+        console.log(`Cached fact id: ${facts_info.id}`);
+      });
+      return { res: fact, status: 200, errorMessage: "" };
+    } catch (err) {
+      return { res: null, status: err.status, errorMessage: err.message };
     }
   }
 }
 
-app.get("/fact", apiLimiter, async (req, res) => {
+app.get("/fact", async (req, res) => {
+  let response_fact = await getFact();
 
-  const factString = await getFacts();
-
-  let response = factString.status !== 200 ? factString.errorMessage : factString.res
-
-  res.status(factString.status).send(response);
+  let message =
+    response_fact.status == 200
+      ? response_fact.res
+      : response_fact.errorMessage;
+  res.status(response_fact.status).send(message);
 });
 
-/*
-app.get('/metar', apiLimiter, async (req,res) => {
+/**
+ * Gets or set the station's metar information
+ * @param {string} station
+ * @returns
+ */
+async function getMetarData(req) {
+  let response_message;
+  const code_station = req.query.station;
+  let metar_key = `metar_${code_station ?? "no_param"}_key`;
+  let metar_string = await redisClient.get(metar_key);
+  if (metar_string !== null) {
+    return { res: JSON.parse(metar_string), status: 200, errorMessage: "" };
+  } else {
+    const parser = new XMLParser();
+    try {
+      let response = await axios.get(
+        `https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${code_station}&hoursBeforeNow=1`
+      );
 
-    let station = req.query.station;
-    let metar;
-    let metarKey = `metar_${station}_key`;
-try {
-    let metarString = await redisClient.get(metarKey);
+      const parsed = parser.parse(response.data);
 
-    if (metarString !==  NULL){
-        metar = JSON.parse(metarString);
-    } else {
-        
-        const parser = new XMLParser();
-        
-        
-            const response = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`);
-
-            if (!response.data) {
-                req.send(500).send(`No hay respuesta del servidor sobre la estacion: ${station}`);
-            }
-            
-            const parsed = parser.parse(response.data);
-
-            if (!parser.response.data.METAR) {
-                req.send(404).send(`No hay información sobre la estacion: ${station}`);
-            }
-
-            metar = decode(parsed.response.data.METAR.raw_text);
-
-            await redisClient.set(metarKey,JSON.stringify(metar),{EX:5});
-            req.send(200).send(metar);
+      if (parsed.response.data.hasOwnProperty("METAR")) {
+        const metar_info = parsed.response.data.METAR;
+        if (metar_info.hasOwnProperty("raw_text")) {
+          response_message = decode(metar_info.raw_text);
+        } else {
+          response_message = metar_info.map((info) => decode(info.raw_text));
+        }
+        redisClient
+          .set(metar_key, JSON.stringify(response_message), { EX: 20 })
+          .then(() => {
+            console.log(`Station ${code_station} cached`);
+          });
+        return {
+          res: response_message,
+          status: 200,
+          errorMessage: "",
+        };
+      } else {
+        return {
+          res: null,
+          status: 404,
+          errorMessage: `Metar no encontro información sobre la estación: ${code_station}`,
+        };
+      }
+    } catch (err) {
+      return { res: null, status: err.status, errorMessage: err.message };
     }
-    } catch(e) {
-        req.send(404).send("");
-    }
+  }
+}
 
-    
-
-}); */
+app.get("/metar", async (req, res) => {
+  let response = await getMetarData(req);
+  console.info(response);
+  let message = response.status == 200 ? response.res : response.errorMessage;
+  res.status(response.status).send(message);
+});
 
 app.listen(3000);
